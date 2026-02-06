9cafc8fb4f20c088af4793408e9e3a1a72e9d18f
gokul101105 <134209814+gokul101105@users.noreply.github.com>
2026-02-06 23:49:41 +0530

    Day 33 - 169. Majority Element.java

--- leetcode/Day 33 - 169. Majority Element.java
+++ leetcode/Day 33 - 169. Majority Element.java
@@ -7,7 +7,7 @@
 - Difficulty:
 - Easy  
 -
-- Problem:
+- Problem:  
 - Given an array `nums` of size `n`, return the element that appears more than ⌊n / 2⌋ times.
 - You may assume that the majority element always exists in the array.
 -
