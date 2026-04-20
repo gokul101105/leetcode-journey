00086601f967bd87d01c62d5708ba98651647f25
gokul101105 <134209814+gokul101105@users.noreply.github.com>
2026-04-20 22:01:13 +0530

    Day 65 - 268. Missing Number.java

--- leetcode/Day 65 - 268. Missing Number.java
+++ leetcode/Day 65 - 268. Missing Number.java
@@ -5,7 +5,7 @@
  * Difficulty: Easy
  * 
  * Problem:
- * Given an array nums containing n distinct numbers in the range [0, n],
+ * Given an array nums containing n distinct numbers in the range [0, n]
  * return the only number in the range that is missing from the array.
  * 
  * LeetCode Link:
